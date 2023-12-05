004292a5e9aa377cc81d79ea95b72ab8cf5b05d4
maheshmarar <139688887+maheshmarar@users.noreply.github.com>
2023-12-05 15:40:45 +0530

    Update pandasai.py

--- pandasai.py
+++ pandasai.py
@@ -1,89 +1,31 @@
-from langchain.agents import AgentType
-from langchain.agents import create_pandas_dataframe_agent
-from langchain.callbacks import StreamlitCallbackHandler
-from langchain.chat_models import ChatOpenAI
 import streamlit as st
+from pandas_ai import AI
 import pandas as pd
-import os
 
-file_formats = {
-    "csv": pd.read_csv,
-    "xls": pd.read_excel,
-    "xlsx": pd.read_excel,
-    "xlsm": pd.read_excel,
-    "xlsb": pd.read_excel,
-}
+# Function to create a chatbot and get response
+def get_chatbot_response(chatbot, message):
+    response = chatbot.ask(message)
+    return response
 
+def main():
+    st.title("CSV Chatbot with PandasAI")
 
-def clear_submit():
-    """
-    Clear the Submit Button State
-    Returns:
+    # Upload CSV file
+    uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+    if uploaded_file is not None:
+        # Read the CSV file
+        data = pd.read_csv(uploaded_file)
 
-    """
-    st.session_state["submit"] = False
+        # Create a chatbot
+        chatbot = AI(data)
 
+        # Chat interface
+        user_input = st.text_input("Ask a question about the data:")
 
-@st.cache_data(ttl="2h")
-def load_data(uploaded_file):
-    try:
-        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
-    except:
-        ext = uploaded_file.split(".")[-1]
-    if ext in file_formats:
-        return file_formats[ext](uploaded_file)
-    else:
-        st.error(f"Unsupported file format: {ext}")
-        return None
+        if user_input:
+            # Get the response from the chatbot
+            response = get_chatbot_response(chatbot, user_input)
+            st.text(f"Response: {response}")
 
-
-st.set_page_config(page_title="LangChain: Chat with pandas DataFrame", page_icon="🦜")
-st.title("🦜 LangChain: Chat with pandas DataFrame")
-
-uploaded_file = st.file_uploader(
-    "Upload a Data file",
-    type=list(file_formats.keys()),
-    help="Various File formats are Support",
-    on_change=clear_submit,
-)
-
-if not uploaded_file:
-    st.warning(
-        "This app uses LangChain's `PythonAstREPLTool` which is vulnerable to arbitrary code execution. Please use caution in deploying and sharing this app."
-    )
-
-if uploaded_file:
-    df = load_data(uploaded_file)
-
-openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
-if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt := st.chat_input(placeholder="What is this data about?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    llm = ChatOpenAI(
-        temperature=0, model="gpt-3.5-turbo-0613", openai_api_key=openai_api_key, streaming=True
-    )
-
-    pandas_df_agent = create_pandas_dataframe_agent(
-        llm,
-        df,
-        verbose=True,
-        agent_type=AgentType.OPENAI_FUNCTIONS,
-        handle_parsing_errors=True,
-    )
-
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        st.write(response)
+if __name__ == "__main__":
+    main()
